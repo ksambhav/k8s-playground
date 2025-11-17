d86247f07e62b523bee5677a2ce4089d412916ff
Kumar Sambhav Jain <kumar.sambhav.jain@gmail.com>
2025-11-17 08:56:28 +0530

    dbt

--- dbt01/elt01/models/example/stg_employee.sql
+++
@@ -1,2 +0,0 @@
-{{ config(materialized='table') }}
-select * from {{source('db02', 'employee')}}
---
+++ dbt01/elt01/models/example/stg_employee_v7.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        pre_hook="set session query_max_run_time='10m'",
+        materialized='table',
+        properties= {
+          "object_store_layout_enabled" : false,
+          "format": "'PARQUET'",
+          "partitioning": "ARRAY['id']",
+          "sorted_by ": "ARRAY['name DESC NULLS FIRST']",
+          "parquet_bloom_filter_columns" : "ARRAY['id']"
+        }
+    )
+}}
+select id, name from {{source('db02', 'employee')}}
